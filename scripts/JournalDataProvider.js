d26bce59721a9638a472570948e1b55aefbc8527
Ember Parr <ember-devops@gmail.com>
2020-08-26 11:54:12 -0500

    hmm

---
+++ scripts/JournalDataProvider.js
@@ -0,0 +1,59 @@
+/*      
+ *   
+ *      Journal data provider for Daily Journal application
+ *
+ *      Holds the raw data about each entry and exports
+ *      functions that other modules can use to filter
+ *      the entries for different purposes.
+ */
+
+// This is the original data.
+const journal = [
+    {
+        id: 1,
+        date: "08/10/2020",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Giddy"
+    },
+    {
+        id: 2,
+        date: "08/11/2020",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+        mood: "sad"
+    },
+    {
+        id: 3,
+        date: "08/12/2020",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+        mood: "Sad"
+    },
+    {
+        id: 4,
+        date: "07/26/2025",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+        mood: "Sad"
+    },
+    {
+        id: 5,
+        date: "07/26/2025",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+        mood: "Sad"
+    }
+]
+
+/*
+    You export a function that provides a version of the
+    raw data in the format that you want
+*/
+export const useJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+    return sortedByDate
+}
