be70eee9e39285e777450c0008101414366c2acd
Ember Parr <ember-devops@gmail.com>
2020-09-18 14:09:38 -0500

    added saveEntry function to data provider

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -20,3 +20,18 @@ export const getJournalEntries = () => {
 export const useJournalEntries = () => {
   return entries.slice();
 };
+
+
+export const saveEntry = (entryObj) => {
+  return fetch("http://localhost:8088/entries", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(noteObj),
+  })
+  .then(() => {
+    return getJournalEntries()
+  })
+  .then(dispatchStateChangeEvent);
+}
