39daf6f798d70001f77f3a4374f81d1b70210103
Ember Parr <ember-devops@gmail.com>
2020-10-01 12:04:00 -0500

    filter functionality works and displays correct entries after mood selected

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 import { JournalList } from "./JournalList.js";
 import { JournalForm, journalMoodSelect } from "./JournalForm.js";
+import { FilterList } from "./filters/FilterList.js";
 
 journalMoodSelect();
 JournalList();
 JournalForm();
+FilterList();
